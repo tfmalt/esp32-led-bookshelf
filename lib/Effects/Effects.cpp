d1c994be48cff9a98d2322411e1036c8113d6a37
Thomas Malt <thomas.malt@knowit.no>
2018-09-29 22:12:13 +0200

    Added all effects and commands to own library.

--- lib/Effects/Effects.cpp
+++ lib/Effects/Effects.cpp
@@ -4,6 +4,9 @@
 Effects::Effects() {
     currentCommand = &Effects::cmdEmpty;
     currentEffect  = &Effects::cmdEmpty;
+
+    currentCommandType = Command::None;
+    currentEffectType  = Effect::NullEffect;
 }
 
 void Effects::setFPS(uint8_t f)
@@ -23,23 +26,81 @@ void Effects::setCommandFrames(uint16_t i)
 
 void Effects::setCurrentCommand(Command cmd)
 {
-    LightState state = lightState->getCurrentState();
+    LightState state   = lightState->getCurrentState();
+    commandFrameCount  = 0;
+    commandStart       = millis();
+    currentCommandType = cmd;
+
     setCommandFrames(FPS * (state.transition || 1));
-    commandFrameCount = 0;
-    commandStart = millis();
 
     switch(cmd) {
         case Command::Brightness :
             currentCommand = &Effects::cmdSetBrightness;
             break;
+        case Command::Color :
+            currentCommand = &Effects::cmdFadeTowardColor;
+            break;
         default :
             currentCommand = &Effects::cmdEmpty;
+            currentCommandType = Command::None;
+    }
+}
+
+void Effects::setCurrentEffect(String effect)
+{
+    setCurrentEffect(getEffectFromString(effect));
+}
+
+Effects::Effect Effects::getEffectFromString(String str)
+{
+    if (str == "Glitter Rainbow")   return Effect::GlitterRainbow;
+    if (str == "Rainbow")           return Effect::Rainbow;
+    if (str == "BPM")               return Effect::BPM;
+    if (str == "Confetti")          return Effect::Confetti;
+    if (str == "Juggle")            return Effect::Juggle;
+    if (str == "Sinelon")           return Effect::Sinelon;
+
+    return Effect::NullEffect;
+}
+
+void Effects::setCurrentEffect(Effect effect)
+{
+    LightState state = lightState->getCurrentState();
+    currentEffectType = effect;
+
+    switch(effect) {
+        case Effect::GlitterRainbow :
+            currentEffect = &Effects::effectGlitterRainbow;
+            break;
+        case Effect::Rainbow :
+            currentEffect = &Effects::effectRainbow;
+            break;
+        case Effect::BPM :
+            currentEffect = &Effects::effectBPM;
+            break;
+        case Effect::Confetti :
+            currentEffect = &Effects::effectConfetti;
+            break;
+        case Effect::Sinelon :
+            currentEffect = &Effects::effectSinelon;
+            break;
+        case Effect::Juggle :
+            currentEffect = &Effects::effectJuggle;
+            break;
+        default :
+            currentEffectType = Effect::NullEffect;
+            currentEffect     = &Effects::cmdEmpty;
     }
 }
 
 void Effects::runCurrentCommand()
 {
     (this->*currentCommand)();
+}
+
+void Effects::runCurrentEffect()
+{
+    (this->*currentEffect)();
 }
 
 void Effects::cmdEmpty()
@@ -65,4 +126,139 @@ void Effects::cmdSetBrightness()
 
         setCurrentCommand(Command::None);
     }
+}
+
+/**
+ * Helper function that blends one uint8_t toward another by a given amount
+ * Taken from: https://gist.github.com/kriegsman/d0a5ed3c8f38c64adcb4837dafb6e690
+ */
+void Effects::nblendU8TowardU8( uint8_t& cur, const uint8_t target, uint8_t amount)
+{
+    if( cur == target) return;
+
+    if( cur < target ) {
+        uint8_t delta = target - cur;
+        delta = scale8_video( delta, amount);
+        cur += delta;
+    } else {
+        uint8_t delta = cur - target;
+        delta = scale8_video( delta, amount);
+        cur -= delta;
+    }
+}
+
+/**
+ * Blend one CRGB color toward another CRGB color by a given amount.
+ * Blending is linear, and done in the RGB color space.
+ * This function modifies 'cur' in place.
+ * Taken from: https://gist.github.com/kriegsman/d0a5ed3c8f38c64adcb4837dafb6e690
+ */
+CRGB Effects::fadeTowardColor( CRGB& cur, const CRGB& target, uint8_t amount)
+{
+    nblendU8TowardU8( cur.red,   target.red,   amount);
+    nblendU8TowardU8( cur.green, target.green, amount);
+    nblendU8TowardU8( cur.blue,  target.blue,  amount);
+    return cur;
+}
+
+/**
+ * Fade an entire array of CRGBs toward a given background color by a given amount
+ * This function modifies the pixel array in place.
+ * Taken from: https://gist.github.com/kriegsman/d0a5ed3c8f38c64adcb4837dafb6e690
+ */
+void Effects::fadeTowardColor( CRGB* L, uint16_t N, const CRGB& bgColor, uint8_t fadeAmount)
+{
+    uint16_t check = 0;
+    for( uint16_t i = 0; i < N; i++) {
+        fadeTowardColor( L[i], bgColor, fadeAmount);
+        if (L[i] == bgColor) check++;
+    }
+
+    if (check == numberOfLeds) {
+        // currentCommand = cmdEmpty;
+        Serial.printf("  - fade towards color done in %lu ms.\n", (millis() - commandStart));
+        setCurrentCommand(Command::None);
+    }
+}
+
+
+void Effects::cmdFadeTowardColor()
+{
+    LightState state = lightState->getCurrentState();
+    CRGB targetColor(state.color.r, state.color.g, state.color.b);
+
+    fadeTowardColor(leds, numberOfLeds, targetColor, 12);
+}
+
+void Effects::setLeds(CRGB *l, const uint8_t &n)
+{
+    numberOfLeds = n;
+    leds = l;
+}
+
+void Effects::effectRainbow()
+{
+    fill_rainbow( leds, numberOfLeds, startHue, 5);
+}
+
+void Effects::addGlitter(fract8 chanceOfGlitter)
+{
+    if( random8() < chanceOfGlitter) {
+        leds[ random16(numberOfLeds) ] += CRGB::White;
+    }
+}
+
+void Effects::effectGlitterRainbow()
+{
+    // built-in FastLED rainbow, plus some random sparkly glitter
+    effectRainbow();
+    addGlitter(80);
+}
+
+void Effects::effectConfetti()
+{
+  // random colored speckles that blink in and fade smoothly
+  fadeToBlackBy( leds, numberOfLeds, 20);
+  int pos = random16(numberOfLeds);
+  leds[pos] += CHSV( startHue + random8(64), 200, 255);
+}
+
+// a colored dot sweeping back and forth, with fading trails
+void Effects::effectSinelon()
+{
+  fadeToBlackBy( leds, numberOfLeds, 96 );
+  LightState state = lightState->getCurrentState();
+
+  int pos = beatsin16(18, 0, numberOfLeds-1 );
+  leds[pos] += CHSV( startHue, 255, 255);
+  // leds[pos] += CRGB(state.color.r, state.color.g, state.color.b);
+}
+
+void Effects::effectBPM()
+{
+    // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
+    uint8_t BeatsPerMinute = 64;
+    CRGBPalette16 palette = PartyColors_p;
+    uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
+    for( int i = 0; i < numberOfLeds; i++) { //9948
+        leds[i] = ColorFromPalette(palette, startHue+(i*2), beat-startHue+(i*10));
+    }
+}
+
+void Effects::effectJuggle() {
+  // eight colored dots, weaving in and out of sync with each other
+  fadeToBlackBy( leds, numberOfLeds, 20);
+  byte dothue = 0;
+  for( int i = 0; i < 8; i++) {
+    leds[beatsin16( i+7, 0, numberOfLeds-1 )] |= CHSV(dothue, 200, 255);
+    dothue += 32;
+  }
+}
+
+Effects::Effect Effects::getCurrentEffect() {
+    return currentEffectType;
+}
+
+void Effects::setStartHue(uint8_t hue) {
+    startHue = hue;
 }
--- lib/Effects/Effects.h
+++ lib/Effects/Effects.h
@@ -7,34 +7,67 @@
 
 class Effects {
     private:
-        typedef void (Effects::*LightCmd)();
-        LightCmd currentCommand;
-        LightCmd currentEffect;
-        LightStateController *lightState;
+        typedef void            (Effects::*LightCmd)();
+        LightCmd                currentCommand;
+        LightCmd                currentEffect;
+        LightStateController    *lightState;
+        CRGB                    *leds;
 
-        uint8_t     FPS                 = 0;
-        uint16_t    commandFrameCount   = 0;
-        uint16_t    commandFrames       = 0;
-        ulong       commandStart        = 0;
+        uint8_t                 FPS                 = 0;
+        uint8_t                 numberOfLeds        = 0;
+        uint8_t                 startHue            = 0;
+        uint16_t                commandFrameCount   = 0;
+        uint16_t                commandFrames       = 0;
+        ulong                   commandStart        = 0;
 
         void cmdEmpty();
         void cmdSetBrightness();
+        void cmdFadeTowardColor();
+        void fadeTowardColor(CRGB *L, uint16_t N, const CRGB &bgColor, uint8_t fadeAmount);
+        CRGB fadeTowardColor(CRGB &cur, const CRGB &target, uint8_t amount);
+        void nblendU8TowardU8(uint8_t &cur, const uint8_t target, uint8_t amount);
+        void addGlitter(fract8 chanceOfGlitter);
+        void effectGlitterRainbow();
+        void effectRainbow();
+        void effectBPM();
+        void effectConfetti();
+        void effectSinelon();
+        void effectJuggle();
+
     public:
         enum Command {
             Null,
             None,
             Empty,
-            Brightness
+            Brightness,
+            Color
         };
         enum Effect {
-
+            Confetti,
+            BPM,
+            GlitterRainbow,
+            Rainbow,
+            Juggle,
+            Sinelon,
+            NullEffect,
+            EmptyEffect,
+            NoEffect
         };
+        Command  currentCommandType;
+        Effect   currentEffectType;
         Effects();
         void setCurrentCommand(Command cmd);
+        void setCurrentEffect(String effect);
+        void setCurrentEffect(Effect effect);
+        Effect getEffectFromString(String str);
         void runCurrentCommand();
+        void runCurrentEffect();
         void setFPS(uint8_t f);
         void setLightStateController(LightStateController *l);
         void setCommandFrames(uint16_t i);
+        void setLeds(CRGB *l, const uint8_t &n);
+        Effect getCurrentEffect();
+        void setStartHue(uint8_t hue);
 };
 
 #endif // Effects_h
