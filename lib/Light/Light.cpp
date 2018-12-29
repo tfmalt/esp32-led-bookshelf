6d9d6aef48818cc84692ac31e0c3ad6042855c4b
Thomas Malt <thomas.malt@knowit.no>
2018-12-29 10:43:01 +0100

    Updated light class to be repsonsible for a segment of light.

--- lib/Light/Light.cpp
+++ lib/Light/Light.cpp
@@ -1,8 +1,27 @@
 #include "Light.h"
-#include <FastLED.h>
 
 Light::Light()
 {
+}
+
+Light::Light(CRGB* l)
+{
+    addLeds(l);
+}
+
+Light::Light(CRGB* l, uint8_t fps, LightConfig lc, String username) {
+    leds = l;
+    FPS  = fps;
+    name = lc.name;
+
+    command_topic = String("/" + username + lc.command_topic);
+    state_topic   = String("/" + username + lc.state_topic);
+    filename      = String("/" + name + "_state.json");
+
+    addSegment(Segment::Top, lc.top[0], lc.top[1]);
+    addSegment(Segment::Bottom, lc.bottom[0], lc.bottom[1]);
+
+    state.initialize(filename.c_str());
 }
 
 Light& Light::addLeds(CRGB* l)
@@ -28,4 +47,40 @@ Light& Light::addSegment(Segment segment, uint16_t start, uint16_t stop)
     }
 
     return *this;
+}
+
+Light& Light::setCommandTopic(const char* topic) {
+    command_topic = topic;
+    return *this;
+}
+
+Light& Light::setStateTopic(const char* topic) {
+    state_topic = topic;
+    return *this;
+}
+
+String Light::getStateTopic()
+{
+    return state_topic;
+}
+
+String Light::getCommandTopic()
+{
+    return command_topic;
+}
+
+uint8_t Light::getBrightness() {
+    return state.getCurrentState().brightness;
+}
+
+String Light::getStateAsJSON()
+{
+    char json[256];
+    state.printStateJsonTo(json);
+    return String(json);
+}
+
+String Light::getName()
+{
+    return name;
 }
--- lib/Light/Light.h
+++ lib/Light/Light.h
@@ -1,6 +1,11 @@
 #ifndef LightController_h
 #define LightController_h
+
+#include <Arduino.h>
 #include <FastLED.h>
+#include <Effects.h>
+#include <LightState.h>
+#include <LedshelfConfig.h>
 
 class Light {
     public:
@@ -12,15 +17,36 @@ class Light {
         };
 
         Light();
+        Light(CRGB* l);
+        Light(CRGB* l, uint8_t fps, LightConfig lc, String username);
 
-        Light& addLeds(CRGB* l);
-        Light& addSegment(Segment segment, uint16_t start, uint16_t stop);
+        Light&  addLeds(CRGB* l);
+        Light&  addSegment(Segment segment, uint16_t start, uint16_t stop);
+        Light&  setCommandTopic(const char* topic);
+        Light&  setStateTopic(const char* topic);
+        Light&  setFPS(uint8_t fps);
+        uint8_t getBrightness();
+        String  getStateAsJSON();
+        String  getName();
+        String  getStateTopic();
+        String  getCommandTopic();
+
     private:
-        CRGB*    leds;
-        uint16_t topStart;
-        uint16_t topStop;
-        uint16_t bottomStart;
-        uint16_t bottomStop;
+        Effects     effects;
+        LightState  state;
+
+        CRGB*       leds;
+        uint8_t     FPS;
+
+        uint16_t    topStart        = 0;
+        uint16_t    topStop         = 0;
+        uint16_t    bottomStart     = 0;
+        uint16_t    bottomStop      = 0;
+
+        String command_topic;
+        String state_topic;
+        String name;
+        String filename;
 };
 
 #endif // LightController_h
